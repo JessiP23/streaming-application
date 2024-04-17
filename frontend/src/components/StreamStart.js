29888873b9091f0e41add3fa9d3306f81846a0f7
Jessi Pavia <jessi316866@gmail.com>
2024-04-16 20:45:57 -0400

    audio and video features

--- frontend/src/components/StreamStart.js
+++ frontend/src/components/StreamStart.js
@@ -1,19 +1,26 @@
 import React, {useState, useRef} from 'react';
 import { v4 as uuid } from 'uuid';
+import './StreamStart.css';
 
 function StreamStart() {
     const [streaming, setStreaming] = useState(false);
     const [transferCode, setTransferCode] = useState('');
     const [stream, setStream] = useState(null);
+    const [sharingScreen, setSharingScreen] = useState(false);
     const videoRef = useRef(null);
 
     //Camera setup video and microphone
     const handleStartStream = async () => {
         try {
-            const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+            let mediaStream;
+            if (sharingScreen) {
+                mediaStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
+            } else {
+                mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+            }
             setStream(mediaStream);
             setStreaming(true);
-            videoRef.current.srcObject = mediaStream;
+            videoRef.current && (videoRef.current.srcObject = mediaStream);
         } catch (error) {
             console.error('Error accessing camera and microphone:', error);
         }
@@ -30,12 +37,24 @@ function StreamStart() {
     return (
         <div>
             <h2>Stream Start</h2>
-            {!streaming ? (
-                <button onClick={handleStartStream}>Start Streaming</button>
-            ) : (
+            <div>
+                {!streaming ? (
+                    <>
+                        <button onClick={handleStartStream}>Start Streaming</button>
+                        <label htmlFor='shareScreen'>Share Screen</label>
+                        <input type='checkbox' id='shareScreen' checked={sharingScreen} onChange={() => setSharingScreen(!sharingScreen)} />
+                    </>
+                ) : (
+                    <div className='video-container'>
+                        <video ref={videoRef} autoPlay />
+                        <button onClick={handleStopStream}>Stop Streaming</button>
+                    </div>
+                )}
+            </div>
+            {transferCode && (
                 <div>
-                    <video ref={videoRef} autoPlay />
-                    <button onClick={handleStopStream}>Stop Streaming</button>
+                    <p>Transfer Code: {transferCode}</p>
+                    <p>Share this code if you desire to transfer the stream to anyone else.</p>
                 </div>
             )}
         </div>
